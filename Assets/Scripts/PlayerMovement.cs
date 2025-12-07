66b12c553c40d0a63c87962c64d96496c02887bc
FishingFella <kieran.hodgson86@gmail.com>
2025-12-07 00:00:35 +0000

    Character Movement
    Player and Camera Movement done,
    Missing changing direction.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.IO.Enumeration;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,19 +6,23 @@ public class PlayerMovement : MonoBehaviour
 {
     [Range(0, 15)]
     public float speed;
-    [Range(0, 10)]
-    public float rotateSpeed;
-    
-    private Vector3 _movementInput = Vector3.zero;
+    [Range(8, 30)]
+    public float sensitivity;
+
+    public float gravityForce = 3;
+
+    private Vector3 _movementInput;
     private Vector3 _lookInput = Vector3.zero;
     
     private CharacterController _characterController;
-    private Camera _camera;
+    private Transform _cameraTransform;
+    private float _currentYRotation = 0f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        _cameraTransform = transform.Find("Main Camera");
         
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -26,20 +31,31 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        _characterController.Move( _movementInput * (speed * Time.deltaTime));
+        _characterController.Move(_movementInput);
 
+        //Camera vertical rotation
+        _currentYRotation -= _lookInput.x;
+        //ensure value is within acceptable range
+        _currentYRotation = Mathf.Clamp(_currentYRotation, -90f, 90f);
+        //Set the rotation
+        _cameraTransform.localRotation = Quaternion.Euler(_currentYRotation, 0f, 0f);
+        
+        //rotates along the horizontal axis
         transform.Rotate(Vector3.up * _lookInput.y);
     }
 
     void OnMove(InputValue value)
     {
-        _movementInput = new  Vector3(value.Get<Vector2>().x, -9.81f, value.Get<Vector2>().y);
+        var moveX = value.Get<Vector2>().x * speed * Time.deltaTime;
+        var moveZ = value.Get<Vector2>().y * speed * Time.deltaTime;
+        
+        _movementInput = new Vector3(moveX, -gravityForce * Time.deltaTime ,moveZ);
     }
 
     void OnLook(InputValue value)
     {
         _lookInput = new  Vector3(value.Get<Vector2>().y, value.Get<Vector2>().x, 0);
-        _lookInput *= rotateSpeed * Time.deltaTime;
+        _lookInput *= sensitivity * Time.deltaTime;
     }
 
     void OnJump()
